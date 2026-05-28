3a0a729a208ac47964c6447d0a9f7833a1418717
MilanVeul <milan@veul.net>
2026-05-28 21:43:43 +0200

    Finished fast implementation

--- question4.py
+++ question4.py
@@ -60,27 +60,30 @@ def solve_blueprint_sdp_slow(num_scanners, wait_cost, operating_costs, discount)
     )
 
 
-def solve_blueprint_sdp_slow(num_scanners, wait_cost, operating_costs, discount):
-    max_state = 500
+def solve_blueprint_sdp_fast(num_scanners, wait_cost, operating_costs, discount):
+    max_state = 1000
+    epsilon = 1e-2
 
     max_d = 10 * num_scanners
     num_states = max_state + 1
     num_actions = max_d + 1
     states = list(range(max_state+1))
 
-    
     lambda_a = 290
 
     # precompute distributions
     print("Precomputing distributions...")
-    max_arrival = max_state + 32 * (10 * num_scanners+1)
-    poisson_pmf = [stats.poisson.pmf(k, lambda_a) for k in range(max_arrival)]
-    poisson_cdf = [stats.poisson.cdf(k, lambda_a) for k in range(max_arrival)]
+    max_arrival = max_state + 32*max_d
+    # poisson_pmf = [stats.poisson.pmf(k, lambda_a) for k in range(max_arrival)]
+    # poisson_cdf = [stats.poisson.cdf(k, lambda_a) for k in range(max_arrival)]
+    poisson_pmf = stats.poisson.pmf(np.arange(max_arrival), lambda_a)
+    poisson_cdf = stats.poisson.cdf(np.arange(max_arrival), lambda_a)
 
     
     # Precompute costs and transition matrices
-    costs = np.full((num_states, num_actions), np.inf) # Invalid actions have infinite costs
-    transitions = np.zeros((num_states, num_states, num_actions))
+    print("Precomputing transitions and cost matrices")
+    costs = np.full((num_states, num_actions), np.inf)              # from,decision   (Invalid actions have infinite costs)
+    transitions = np.zeros((num_states, num_actions, num_states))   # from,decision,to
 
     for i in states:
         min_d = int(np.ceil(i/32.0))
@@ -97,18 +100,21 @@ def solve_blueprint_sdp_slow(num_scanners, wait_cost, operating_costs, discount)
             costs[i, d] = (7*i + 2.5*expectation)*wait_cost + d*operating_costs
 
             # Compute transitions
-            transitions[0, i, d] = poisson_cdf[M]
+            transitions[i, d, 0] = poisson_cdf[M]
             j_vals = np.arange(1, max_state)
-            transitions[j_vals, i, d] = poisson_pmf[M + j_vals]
-            transitions[max_state, i, d] = 1 - poisson_cdf[M + max_state - 1]
+            transitions[i, d, j_vals] = poisson_pmf[M + j_vals]
+            transitions[i, d, max_state] = 1 - poisson_cdf[M + max_state - 1]
     
     return fast_discounted_value_iteration(
         num_states=num_states,
-        
+        costs=costs,
+        transitions=transitions,
+        discount=discount,
+        epsilon=epsilon
     )
 
 
 
 
 if __name__ == "__main__":
-    solve_blueprint_sdp_slow(num_scanners=20, wait_cost=15, operating_costs=200, discount=0.8)
+    optimal_values, optimal_policy, iterations = solve_blueprint_sdp_fast(num_scanners=20, wait_cost=15, operating_costs=200, discount=0.8)
--- solvers/value_iteration.py
+++ solvers/value_iteration.py
@@ -42,13 +42,14 @@ def discounted_value_iteration(
             delta = max(delta, abs(v_new[i] - v[i]))
         v = v_new
 
-        print(f"Finished iteration {iter:<5} delta={delta}")
+        print(f"Iteration {iter:<4} Delta: {delta:.6e}")
+
         if delta < epsilon:
             break
     else:
         raise Exception("Reached maximum iterations before converging.")
     
-    return v, policy
+    return v, policy, iter
 
 
 def fast_discounted_value_iteration(
@@ -58,12 +59,34 @@ def fast_discounted_value_iteration(
         discount: float,
         epsilon: float =  1e-6,
         max_iterations: int = 1000
-    ):
+    ) -> Tuple[dict[int, float], dict[int, int], int]:
+    # Initiate values and policy
     v = np.zeros(num_states)
     policy = np.zeros(num_states, dtype=int)
 
     for iter in range(max_iterations):
-        expectation = np.dot(transitions, v)
-        v_values = costs + discount * expectation
+        # Docs: "If a is an N-D array and b is a 1-D array, it is a sum product over the last axis of a and b"
+        # so this is a dot product of probabilities col and value col
+        expectation = np.dot(transitions, v)        # 2d array
+        v_values = costs + discount * expectation   # 2d array (rows are the decisions, columns are states)
 
-        v_new = np.min(v_values, axis=1)
+        v_new = np.min(v_values, axis=1)            # 1d array (get the minimum of over columns (=decisions))
+        delta = np.max(np.abs(v_new - v))           # 0d array (Single value)
+
+        v = v_new
+
+        converged = delta < epsilon
+
+        if iter % 10 == 0 or converged:
+            print(f"Iteration {iter:<4} Delta: {delta:.6e}")
+
+        if converged:
+            policy = np.argmin(v_values, axis=1)    # Get decisions (row) that minimizes the columns
+            break
+    else:
+        raise Exception("Reached maximum iterations before converging.")
+    
+    # Construct easy-to-use dicts
+    v_dict = {i: v[i] for i in range(num_states)}
+    policy_dict = {i: policy[i] for i in range(num_states)}
+    return v_dict, policy_dict, iter
